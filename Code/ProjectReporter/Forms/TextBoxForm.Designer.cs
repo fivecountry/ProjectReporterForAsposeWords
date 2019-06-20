18ee41c1ce214de1c20b0cb2dfb77fc77d775f71
wcss <wcss2020@gmail.com>
2019-06-20 21:37:00 +0800

    ...

---
+++ Code/ProjectReporter/Forms/TextBoxForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ProjectReporter.Forms
+{
+    partial class TextBoxForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plButtons = new DevExpress.XtraEditors.PanelControl();
+            this.btnOK = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.btnCancel = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.plContent = new DevExpress.XtraEditors.PanelControl();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.plButtons)).BeginInit();
+            this.plButtons.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.plContent)).BeginInit();
+            this.plContent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plButtons
+            // 
+            this.plButtons.Controls.Add(this.btnOK);
+            this.plButtons.Controls.Add(this.btnCancel);
+            this.plButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.plButtons.Location = new System.Drawing.Point(2, 327);
+            this.plButtons.Name = "plButtons";
+            this.plButtons.Size = new System.Drawing.Size(619, 49);
+            this.plButtons.TabIndex = 7;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnOK.Location = new System.Drawing.Point(437, 2);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(90, 45);
+            this.btnOK.StateCommon.Content.LongText.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOK.StateCommon.Content.ShortText.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOK.TabIndex = 17;
+            this.btnOK.TabStop = false;
+            this.btnOK.Values.Text = "保存";
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnCancel.Location = new System.Drawing.Point(527, 2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(90, 45);
+            this.btnCancel.StateCommon.Content.LongText.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCancel.StateCommon.Content.ShortText.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.TabStop = false;
+            this.btnCancel.Values.Text = "取消";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // plContent
+            // 
+            this.plContent.Controls.Add(this.txtContent);
+            this.plContent.Controls.Add(this.plButtons);
+            this.plContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plContent.Location = new System.Drawing.Point(0, 0);
+            this.plContent.Name = "plContent";
+            this.plContent.Size = new System.Drawing.Size(623, 378);
+            this.plContent.TabIndex = 11;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtContent.Location = new System.Drawing.Point(2, 2);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(619, 325);
+            this.txtContent.TabIndex = 8;
+            // 
+            // TextBoxForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 378);
+            this.Controls.Add(this.plContent);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TextBoxForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "详细内容";
+            ((System.ComponentModel.ISupportInitialize)(this.plButtons)).EndInit();
+            this.plButtons.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.plContent)).EndInit();
+            this.plContent.ResumeLayout(false);
+            this.plContent.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl plButtons;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnOK;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnCancel;
+        private DevExpress.XtraEditors.PanelControl plContent;
+        private System.Windows.Forms.TextBox txtContent;
+    }
+}
