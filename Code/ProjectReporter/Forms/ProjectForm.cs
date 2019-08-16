7c8775a9a82b255c79d620929719f3e56a61f859
wcss <wcss2020@gmail.com>
2019-08-16 13:44:07 +0800

    ...

--- Code/ProjectReporter/Forms/ProjectForm.cs
+++ Code/ProjectReporter/Forms/ProjectForm.cs
@@ -84,7 +84,7 @@ namespace ProjectReporter.Forms
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void btnLoad_Click(object sender, EventArgs e)
@@ -94,28 +94,63 @@ namespace ProjectReporter.Forms
                 //新项目目录
                 string newProjectDir = System.IO.Path.Combine(MainForm.BaseDir, Guid.NewGuid().ToString());
 
-                try
+                SuperCodeFactoryUILib.Forms.CircleProgressBarDialog dialog = new SuperCodeFactoryUILib.Forms.CircleProgressBarDialog();
+                dialog.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                dialog.TransparencyKey = dialog.BackColor;
+                dialog.ProgressBar.ForeColor = Color.Red;
+                dialog.MessageLabel.ForeColor = Color.Blue;
+                dialog.Start(new EventHandler<SuperCodeFactoryUILib.Forms.CircleProgressBarEventArgs>(delegate(object thisObj, SuperCodeFactoryUILib.Forms.CircleProgressBarEventArgs args)
                 {
-                    //创建目录
                     try
                     {
-                        System.IO.Directory.CreateDirectory(newProjectDir);
+                        dialog.ReportProgress(20, 100);
+                        dialog.ReportInfo("创建导入目录");
+                        try
+                        {
+                            System.Threading.Thread.Sleep(1000);
+                        }
+                        catch (Exception ex) { }
+
+                        //创建目录
+                        try
+                        {
+                            System.IO.Directory.CreateDirectory(newProjectDir);
+                        }
+                        catch (Exception ex) { }
+
+                        dialog.ReportProgress(40, 100);
+                        dialog.ReportInfo("准备解包");
+
+                        //解压需要导入的包                        
+                        new ProjectReporter.Utility.FileZipOpr().UnZipFile(ofdSelect.FileName, newProjectDir, string.Empty, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        dialog.ReportProgress(90, 100);
+                        dialog.ReportInfo("解包出错，正在删除导入目录");
+                        try
+                        {
+                            System.Threading.Thread.Sleep(1000);
+                        }
+                        catch (Exception exx) { }
+
+                        if (System.IO.Directory.Exists(newProjectDir))
+                        {
+                            System.IO.Directory.Delete(newProjectDir);
+                        }
+                    }
+
+                    dialog.ReportProgress(98, 100);
+                    dialog.ReportInfo("刷新列表");
+                    try
+                    {
+                        System.Threading.Thread.Sleep(1000);
                     }
                     catch (Exception ex) { }
 
-                    //解压需要导入的包                        
-                    new ProjectReporter.Utility.FileZipOpr().UnZipFile(ofdSelect.FileName, newProjectDir, string.Empty, true);
-                }
-                catch (Exception ex)
-                {
-                    if (System.IO.Directory.Exists(newProjectDir))
-                    {
-                        System.IO.Directory.Delete(newProjectDir);
-                    }
-                }
-                
-                //更新列表
-                updateProjects();
+                    //更新列表
+                    updateProjects();
+                }));                
             }
         }
     }
